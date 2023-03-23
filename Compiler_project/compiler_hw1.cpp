fa1d20de7c1cfc3ddc3154d00fe235fb18590d8e
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-03-23 21:49:40 +0900

    Understanding the Structure

--- Compiler_project/compiler_hw1.cpp
+++ Compiler_project/compiler_hw1.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>	// exit 함수 라이브러리
 
-char lookahead;
+char lookahead;	// 입력된 문자
 void nexttoken();
 void seq();
 
 void main() {
 	nexttoken();
 	seq();
-	if (lookahead == '$')
+	if (lookahead == '$')	// 마지막이 $면
 		printf("\n");
 	else
 		printf("error\n");
@@ -28,12 +28,18 @@ void nexttoken() {
 
 	while (1) {
 		c = getchar();
-		if (c == EOF) {
+		if (c == EOF) {	// ctrl + z할 경우
 			lookahead = '$';
 			return;
 		}
 		if (c == ' ' || c == '\t' || c == '\n' || c == '\0') continue;
+
 		lookahead = c;
+		printf("%c", lookahead);
+
+		// $시 출력되게 짜야함
+		match(lookahead);	// getchar로 받아온 것에서 문자 하나씩 match로 넘어감
+
 		return;
 	}
 }
