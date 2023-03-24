3fd372fc800f500b9649a1f8a0ec8c1b361a7e7a
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-03-24 18:02:44 +0900

    Output Position

--- Compiler_project/compiler_hw1.cpp
+++ Compiler_project/compiler_hw1.cpp
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>	// exit 함수 라이브러리
+#include <string.h>
 
 char lookahead;	// 입력된 문자
-int x, y = 0;	// 좌표 설정
+int size = 10;
+int** look_p = (int**)malloc(sizeof(int*) * size);	// 입력 저장할 배열
+int x, y, count = 0;	// 좌표 설정
+
 
 void nexttoken();
 void seq();
@@ -12,10 +16,12 @@ void main() {
 	printf("입력:");
 	nexttoken();
 	seq();	// 좌표 계산
+	printf("\n");
+
 	if (lookahead == '$')	// 마지막이 $면
 	{;
-		printf("출력:");
-		position();
+		
+		free(look_p);	// 배열 메모리 해제
 	}
 	else
 		printf("error\n");
@@ -48,6 +54,7 @@ void nexttoken() {
 			lookahead = '$';
 			return;
 		}
+		
 		seq();	// 좌표 계산
 		
 	}
@@ -55,6 +62,8 @@ void nexttoken() {
 
 void seq() {// 좌표 계산
 	// nexttoken에서 저장된 배열을 보면서 좌표 계산하기
+
+
 	switch (lookahead) {
 	case 'e':
 		x = x + 1;
@@ -69,9 +78,15 @@ void seq() {// 좌표 계산
 		y = y - 1;
 		break;
 	}
+	if (lookahead != '$') {
+		count++;
+		look_p[count] = (int*)malloc(sizeof(int) * 2);
+		look_p[count][0] = x;	// x 좌표 저장
+		look_p[count][1] = y;	// y 좌표 저장
+		printf("\(%d,%d\) ", look_p[count][0], look_p[count][1]);	// $까지 출력되는 상황
+	}
+	
 	return;	// match로 이동
 }
 
-void position() {	// 좌표 출력
-	printf("(%d,%d)", x, y);
-}
+// 해결해야할것, 에러 상황 추가 및 출력 문구 위치 지정
