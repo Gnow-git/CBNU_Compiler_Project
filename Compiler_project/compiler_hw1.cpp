1bcc89d301ba2e17483b7b0a81f4891188f74868
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-03-25 15:43:15 +0900

    Error when not starting with b

--- Compiler_project/compiler_hw1.cpp
+++ Compiler_project/compiler_hw1.cpp
@@ -5,82 +5,94 @@
 char lookahead;	// 입력된 문자
 int size = 10;
 int** look_p = (int**)malloc(sizeof(int*) * size);	// 입력 저장할 배열
-int x, y, count = 0;	// 좌표 설정
+int x, y, count,start = 0;	// 좌표 설정
 
 
 void nexttoken();
+void instr(char lookahead);
 void seq();
-void position();	// 위치 출력
 
 void main() {
 	printf("입력:");
 	nexttoken();
-	seq();	// 좌표 계산
-	printf("\n");
-
+	seq();
 	if (lookahead == '$')	// 마지막이 $면
 	{;
 		
 		free(look_p);	// 배열 메모리 해제
 	}
 	else
-		printf("error\n");
+		printf("main error\n");
 }
 
 void match(char token) {
 	if (lookahead == token) {
+		instr(token);
 		nexttoken();
 	}
 	else {
-		printf("error\n");
+		printf("match error\n");
 		exit(1);	// 프로세스 종료
 	}
 }
 
 void nexttoken() {
 	char c;
+
 	while (1) {
 		c = getchar();	// 문자 입력
 		if (c == EOF) {	// ctrl + z할 경우
-			lookahead = '$';
-			return;
+				lookahead = '$';
+				return;
 		}
 		if (c == ' ' || c == '\t' || c == '\n' || c == '\0') continue;
-
 		lookahead = c;
-
-
-		if (c == '$') {	// $입력시 main으로 이동
-			lookahead = '$';
-			return;
-		}
 		
-		seq();	// 좌표 계산
-		
+			seq();
+		return;
 	}
 }
 
-void seq() {// 좌표 계산
+void seq() {
+	
+	if (start == 0 && lookahead == 'b') { // b로 시작했는지 파악
+		start = 1;
+	}
+	else if (start == 1) {
+		if (lookahead == '$') {	// $입력시 main으로 이동
+			return;
+		}
+		match(lookahead);
+	}
+	else {
+		return;
+	};
+
+}
+
+void instr(char lookahead) {// 좌표 계산
 	// nexttoken에서 저장된 배열을 보면서 좌표 계산하기
 
 
 	switch (lookahead) {
-	case 'e':
+	case 'e':		// east
 		x = x + 1;
 		break;
-	case 'n':
+	case 'n':		// north
 		y = y + 1;
 		break;
-	case 'w':
+	case 'w':		// west
 		x = x - 1;
 		break;
-	case 's':
+	case 's':		// south
 		y = y - 1;
 		break;
 	}
+
 	if (lookahead != '$') {
 		count++;
 		look_p[count] = (int*)malloc(sizeof(int) * 2);
+
 		look_p[count][0] = x;	// x 좌표 저장
 		look_p[count][1] = y;	// y 좌표 저장
 		printf("\(%d,%d\) ", look_p[count][0], look_p[count][1]);	// $까지 출력되는 상황
